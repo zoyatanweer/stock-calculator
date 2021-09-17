21fad4dbc1d85558c1d0e2cff27134846cff51e4
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2021-09-17 23:37:08 +0530

    stock-calculator
    Enter the required fields to know whether you gained profit or suffered loss in both absolute value and percentage.

---
+++ README.md
@@ -0,0 +1,3 @@
+**Stock Calculator**
+
+Enter the initial value, quantity and the current value of your stocks. Click on 'Tell me' button to know whether you earned a profit or had a loss based on the stocks. This also tells the profit/loss percent accordingly.
---
+++ app.js
@@ -0,0 +1,52 @@
+//selecting elements
+var initialPrice = document.querySelector('#initial-price');
+var stocksQuantity = document.querySelector('#stocks-quantity');
+var currentPrice = document.querySelector('#current-price');
+var submitBtn = document.querySelector('#submit-btn');
+var output = document.querySelector('#output');
+
+submitBtn.addEventListener("click", submitHandler);
+
+function submitHandler(){
+    var ip = Number(initialPrice.value);
+    var qty = Number(stocksQuantity.value);
+    var curr = Number(currentPrice.value); 
+    calculateProfitAndLoss(ip,qty,curr);
+}
+
+function calculateProfitAndLoss(initial, quantity, current){
+    if(initial > current){
+        // var loss = (initial-current) * quantity;
+        // var lossPercentage = (loss/initial)*100;
+
+        var loss = parseFloat((initial - current) * quantity).toFixed(2);  
+        var lossPercentage = parseFloat((loss/initial) * 100).toFixed(2);
+      
+        if(lossPercentage > 50){
+             document.body.style.backgroundImage = "url('photo/photo3.jpg')";
+        }else{
+             document.body.style.backgroundImage = "url('photo/photo2.jpg')";
+        }
+
+        showOutput(`The loss is ${loss} and the absolute Loss percent is ${lossPercentage}% 😱📉`)
+    }else if(current > initial){
+        // var profit = (current - initial) * quantity;
+        // var profitPercentage = (profit/initial)*100;
+
+        var profit = parseFloat((current - initial) * quantity).toFixed(2);  
+        var profitPercentage = parseFloat((profit/initial) * 100).toFixed(2);
+
+        document.body.style.backgroundImage = "url('photo/photo4.jpg')";
+        showOutput(`Yay! You had absolute profit of ${profit} and the absolute Profit percent is ${profitPercentage}% 🤑📈`)
+    }else if(initial = current){
+        document.body.style.backgroundImage = "url('photo/photo1.jpg')";
+        showOutput("No pain no gain and no gain no pain 🤷")
+    }else{
+        alert("Please enter all the fields 😠")
+    }
+    
+}
+
+function showOutput(message){
+    output.innerText = message;
+}

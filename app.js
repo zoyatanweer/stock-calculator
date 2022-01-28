fc003922b4d4f3dfa568f5ab7f649a2befc91f5f
Zoya Tanweer <87615284+zoyatanweer@users.noreply.github.com>
2022-01-28 11:26:15 +0530

    refactor
    Converted into ES6 syntax

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 //selecting elements
-var initialPrice = document.querySelector('#initial-price');
-var stocksQuantity = document.querySelector('#stocks-quantity');
-var currentPrice = document.querySelector('#current-price');
-var submitBtn = document.querySelector('#submit-btn');
-var output = document.querySelector('#output');
+const initialPrice = document.querySelector('#initial-price');
+const stocksQuantity = document.querySelector('#stocks-quantity');
+const currentPrice = document.querySelector('#current-price');
+const submitBtn = document.querySelector('#submit-btn');
+const output = document.querySelector('#output');
 
 submitBtn.addEventListener("click", submitHandler);
 
-function submitHandler(){
-    var ip = Number(initialPrice.value);
-    var qty = Number(stocksQuantity.value);
-    var curr = Number(currentPrice.value); 
+const submitHandler = () => {
+    const ip = Number(initialPrice.value);
+    const qty = Number(stocksQuantity.value);
+    const curr = Number(currentPrice.value); 
     calculateProfitAndLoss(ip,qty,curr);
 }
 
-function calculateProfitAndLoss(initial, quantity, current){
+const calculateProfitAndLoss = (initial, quantity, current) => {
     if(initial=="" || quantity=="" || current==""){
         showOutput("PLease enter all the fields 😠")
     }
-    else if(initial > current){
-        // var loss = (initial-current) * quantity;
-        // var lossPercentage = (loss/initial)*100;
+    else if(initial > current) {
 
         var loss = parseFloat((initial - current) * quantity).toFixed(2);  
         var lossPercentage = parseFloat(((initial-current) *100)/initial).toFixed(2);
@@ -33,8 +31,6 @@ function calculateProfitAndLoss(initial, quantity, current){
 
         showOutput(`The loss is ${loss} and the absolute Loss percent is ${lossPercentage}% 😱📉`)
     }else if(current > initial){
-        // var profit = (current - initial) * quantity;
-        // var profitPercentage = (profit/initial)*100;
 
         var profit = parseFloat((current - initial) * quantity).toFixed(2);  
         var profitPercentage = parseFloat(((current-initial) * 100)/initial).toFixed(2);
@@ -48,6 +44,4 @@ function calculateProfitAndLoss(initial, quantity, current){
     
 }
 
-function showOutput(message){
-    output.innerText = message;
-}
+const showOutput = (message) => output.innerText = message;
